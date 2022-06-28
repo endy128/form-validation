ca4131b14316ddb551bec824a19a5f5624752633
Neil Douglas <endy64@icloud.com>
2022-06-28 09:57:13 +0100

    Added per input field checking and full check on submit button

--- src/index.js
+++ src/index.js
@@ -1,51 +1,112 @@
-// const form = document.getElementsByTagName("form")[0];
 const form = document.getElementById("form");
-console.log(form);
-
 const email = document.getElementById("email");
-const emailError = document.querySelector("#email + span.error");
+const country = document.getElementById("country");
+const postcode = document.getElementById("postcode");
+const password = document.getElementById("password");
+const password2 = document.getElementById("password2");
 
-email.addEventListener("input", (event) => {
-  // Each time the user types something, we check if the
-  // form fields are valid.
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-  if (email.validity.valid) {
-    // In case there is an error message visible, if the field
-    // is valid, we remove the error message.
-    emailError.textContent = ""; // Reset the content of the message
-    emailError.className = "error"; // Reset the visual state of the message
+  validateInputs();
+});
+
+const inputs = Array.from(document.querySelectorAll("input"));
+inputs.forEach((input) => input.addEventListener("change", (e) => {
+  console.log(e.target.id);
+  if (e.target.id === "email") validateEmail();
+  if (e.target.id === "country") validateCountry();
+  if (e.target.id === "postcode") validatePostcode();
+  if (e.target.id === "password") validatePassword();
+  if (e.target.id === "password2") validatePassword2();
+}));
+
+const setError = (element, message) => {
+  const inputControl = element.parentElement;
+  const errorDisplay = inputControl.querySelector(".error");
+
+  errorDisplay.innerText = message;
+  inputControl.classList.add("error");
+  inputControl.classList.remove("success");
+};
+
+const setSuccess = (element) => {
+  const inputControl = element.parentElement;
+  const errorDisplay = inputControl.querySelector(".error");
+
+  errorDisplay.innerText = "";
+  inputControl.classList.add("success");
+  inputControl.classList.remove("error");
+};
+
+const isValidEmail = (email) => {
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+};
+
+const isValidPostcode = (postcode) => {
+  const re = /^(\d{5}((|-)-\d{4})?)|([A-Za-z]\d[A-Za-z][\s\.\-]?(| -)\d[A-Za-z]\d)|[A-Za-z]{1,2}\d{1,2}[A-Za-z]? \d[A-Za-z]{2}$/;
+  return re.test(String(postcode).toUpperCase());
+};
+
+const validateEmail = () => {
+  const emailValue = email.value.trim();
+
+  if (emailValue === "") {
+    setError(email, "Email is required");
+  } else if (!isValidEmail(emailValue)) {
+    setError(email, "Provide a valid email address");
   } else {
-    // If there is still an error, show the correct error
-    showError();
+    setSuccess(email);
   }
-});
+};
 
-form.addEventListener("submit", (event) => {
-  // if the email field is valid, we let the form submit
-
-  if (!email.validity.valid) {
-    // If it isn't, we display an appropriate error message
-    showError();
-    // Then we prevent the form from being sent by canceling the event
-    event.preventDefault();
+const validateCountry = () => {
+  const countryValue = country.value.trim();
+  if (countryValue === "") {
+    setError(country, "Country is required");
+  } else {
+    setSuccess(country);
   }
-});
+};
 
-function showError() {
-  if (email.validity.valueMissing) {
-    // If the field is empty,
-    // display the following error message.
-    emailError.textContent = "You need to enter an e-mail address.";
-  } else if (email.validity.typeMismatch) {
-    // If the field doesn't contain an email address,
-    // display the following error message.
-    emailError.textContent = "Entered value needs to be an e-mail address.";
-  } else if (email.validity.tooShort) {
-    // If the data is too short,
-    // display the following error message.
-    emailError.textContent = `Email should be at least ${email.minLength} characters; you entered ${email.value.length}.`;
+const validatePostcode = () => {
+  const postcodeValue = postcode.value.trim();
+  if (postcodeValue === "") {
+    setError(postcode, "Postcode is required");
+  } else if (!isValidPostcode(postcodeValue)) {
+    setError(postcode, "Provide a valid postcode");
+  } else {
+    setSuccess(postcode);
   }
+};
 
-  // Set the styling appropriately
-  emailError.className = "error active";
-}
+const validatePassword = () => {
+  const passwordValue = password.value.trim();
+  if (passwordValue === "") {
+    setError(password, "Password is required");
+  } else if (passwordValue.length < 8) {
+    setError(password, "Password must be at least 8 character.");
+  } else {
+    setSuccess(password);
+  }
+};
+
+const validatePassword2 = () => {
+  const password2Value = password2.value.trim();
+  if (password2Value === "") {
+    setError(password2, "Please confirm your password");
+  } else if (password2Value !== passwordValue) {
+    setError(password2, "Passwords doesn't match");
+  } else {
+    setSuccess(password2);
+  }
+};
+
+const validateInputs = () => {
+  validateEmail();
+  validateCountry();
+  validatePostcode();
+  validatePassword();
+  validatePassword2();
+};
